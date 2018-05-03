508dcc14d85a3e3bf91845bf728f0ca71bbc51d1
Nicholas Powers <njpowers7915@gmail.com>
2018-05-03 00:56:03 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  count = 0
+  board.each do |square|
+    if square == "X" || square == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 == 1
+    return "O"
+  elsif count % 2 == 0
+    return "X"
+  end
+end
