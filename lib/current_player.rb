3a4d153033a32264f70eb2b25636ba1bffc4c25f
Nicholas Powers <njpowers7915@gmail.com>
2018-05-03 00:53:46 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,10 @@
 def turn_count(board)
   count = 0
-  board.each { |square|
+  board.each do |square|
     if square == "X" || square == "O"
       count += 1
     end
-  }
+  end
   return count
 end
 
